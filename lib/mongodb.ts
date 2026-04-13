499fe5d0a28a79bc1e0f37f3f53ff3fce304f111
Davideif <dpmcantabria1994@gmail.com>
2026-04-13 11:38:49 +0100

    Bugs fixed, mongodb.ts (import models)

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,22 +1,23 @@
 import mongoose from 'mongoose';
 
-
-let isConnected = false;
-const uri = process.env.MONGODB_URI;
-if (!uri) throw new Error("MONGODB_URI is not defined");
-
-const connectDB: () => Promise<void> = async () => {
-    if (isConnected) {
+const connectDB = async (): Promise<void> => {
+    if (mongoose.connection.readyState >= 1) {
         console.log('DB is already connected');
         return;
     }
 
+    const uri = process.env.MONGODB_URI;
+    if (!uri) throw new Error('MONGODB_URI is not defined');
+
     try {
         await mongoose.connect(uri);
-        isConnected = true;
-        console.log('MongoDB is connected...');
+        
+        await import('@/models/User');
+        await import('@/models/Post');
+        console.log('MongoDB connected...');
     } catch (error) {
         console.error('MongoDB connection error:', error);
+        throw error; 
     }
 };
 
